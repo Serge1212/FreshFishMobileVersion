f80e226f1b9267678e8d908e2aa887fc956573bd
serge1212 <sanichace@gmail.com>
2020-04-30 00:45:34 +0300

    added 3 pages and icon for ProductsPage

--- FreshFishMobile.Android/Resources/drawable/MainPage.xaml.cs
+++ FreshFishMobile.Android/Resources/drawable/MainPage.xaml.cs
@@ -11,15 +11,12 @@ namespace FreshFishMobile
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
 
-            MessagingCenter.Send(this, "hello");
-
-            int i;
 
         }
     }
---
+++ FreshFishMobile/Pages/WorkersPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FreshFishMobile.Pages
+{
+    public partial class WorkersPage : ContentPage
+    {
+        public WorkersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
